82e51bf3945223f5a4ce8139cb4de89f52670f36
Jeremiah Kendl <76667147+juicyjerrry@users.noreply.github.com>
2022-08-08 18:50:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -20,10 +20,70 @@ var answerB = document.querySelector("#answerB")
 var answerC = document.querySelector("#answerC")
 var answerD = document.querySelector("#answerD")
 
+var startButton = document.querySelector("#startButton")
+
+
+var questionArray = [
+    {
+        question: "Question 1 Answer A",
+        choices: ["answer a", "answer b", "answer c", "answer d"],
+        correctAnswer: "Answer a",
+    },
+    {        
+        question: "Question 2 Answer B",
+        choices: ["answer a", "answer b", "answer c", "answer d"],
+        correctAnswer: "Answer b",
+
+    },
+
+    {   
+        question: "Question 3 Answer C",
+        choices: ["answer a", "answer b", "answer c", "answer d"],
+        correctAnswer: "Answer c",
+
+    },
+
+    {
+        question: "Question 4 Answer D",
+        choices: ["answer a", "answer b", "answer c", "answer d"],
+        correctAnswer: "Answer d",
+
+    },
+]; 
+
 var qContainer = document.querySelector(".qContainer")
 
 var secondsLeft = 30;
 var scoreCount = 0;
+var i = 0;
+
+
+//❓ Show Question
+function showQuestion() {
+    question.textContent = questionArray[i].question;
+    answerA.textContent = questionArray[i].choices[0];
+    answerB.textContent = questionArray[i].choices[1];
+    answerC.textContent = questionArray[i].choices[2];
+    answerD.textContent = questionArray[i].choices[3];
+    answerA.addEventListener("click", nextQuestion)
+    answerB.addEventListener("click", nextQuestion)
+    answerC.addEventListener("click", nextQuestion)
+    answerD.addEventListener("click", nextQuestion)
+
+}
+
+//Next Question
+
+function nextQuestion() {
+    console.log(this.innerHTML)
+    // i++;
+    // showQuestion();
+    // if (i >= 3){
+    //     showScoreBoard()
+    // };
+}
+//how do i know which one is the correct answer to the question and create a wrong or right result depending on it
+
 
 //🏀 Score!
 function showScore() {
@@ -50,6 +110,19 @@ function startClock() {
 
 
 // 🔳 Button Function
+// 🏁 Start Button
+startButton.addEventListener("click", function(event)
+{
+    var header =  document.querySelector("#header");
+    var hide = document.querySelector(".qContainer");
+
+   hide.classList.remove("hide");
+   header.classList.add("hide");
+
+   showScore();
+   startClock();
+   showQuestion();
+})
 // to do:
 // Set correct answer ?????
 // on correct answer add 3 seconds and add 1 point
@@ -60,22 +133,13 @@ answerA.addEventListener("click", function(event)
 {
 event.preventDefault();
 
-//conceptually, how do I set a value to be correct and not correct
-if (value === true) {
-    secondsLeft = secondsLeft + 3;
-    scoreCount = scoreCount + 1;
-};
-
-if (value === false) {
-    renderIncorrectMessage()
-};
-
 })
 
 //❌❌❌ wrong answer
 function renderIncorrectMessage() {
     result.textContent = "Incorrect, please try again"
 }
+
 
 //💯 Scoreboard!
 function showScoreBoard() {
@@ -91,6 +155,3 @@ function showScoreBoard() {
 scoreBoard.addEventListener("click", function(event){
     showScoreBoard();
 });
-
-showScore();
-startClock();
